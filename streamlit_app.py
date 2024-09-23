1ebd242182b9fb83927f6b033bb4fced02ff1e13
kate <katherine.spirydovich@gmail.com>
2024-09-23 20:44:32 +0000

    hi

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Happy Birthday Daddy!🥳")
+st.video("https://www.youtube.com/watch?v=xY9G3D1p9D8",autoplay=True)
+st.text("we are so happy that you were born!")
+st.image("https://i.giphy.com/11sBLVxNs7v6WA.webp")
+
+st.image("https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExNXQ2N2pmczNubWZlYXBrNDZrbHFxOHJvMjZpZXBlMDMyN201YjlpZiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/HyanD1KpfzPiw/200.webp")
