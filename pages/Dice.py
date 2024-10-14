ae6e40ebabe1771e6e657413849db4bcbdbacf63
kate <katherine.spirydovich@gmail.com>
2024-10-14 21:04:00 +0000

    bob is amazing

--- pages/Dice.py
+++ pages/Dice.py
@@ -4,13 +4,14 @@ import random
 
 
 # HOMEWORK: Make the get_roll function work!
-
-
-
+def get_roll(num_sides):
+    first_roll=random.randint(1,num_sides)
+    second_roll=random.randint(1,num_sides)
+    roll=(first_roll+second_roll)
+    return roll
 st.write("Roll two dice and get the result")
 
 num_sides = st.slider("How many sides should your dice have?", min_value=2, max_value=20)
-get_roll = random.randint[(1,7)]
 
 roll = get_roll(num_sides)
 
