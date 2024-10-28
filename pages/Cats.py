6885b014fc9c9485b8b2072307c6179724895112
kate <katherine.spirydovich@gmail.com>
2024-10-28 21:47:12 +0000

    Nice class!

--- pages/Cats.py
+++ pages/Cats.py
@@ -1,4 +1,11 @@
-import pyjokes
 import streamlit as st
+import pyjokes
+import requests
 
-st.write(pyjokes.get_joke())
+st.write("# requests")
+
+response = requests.get("https://pokeapi.co/api/v2/pokemon/12/")
+st.image(response.json()["sprites"]["front_default"])
+
+
+# st.write(pyjokes.get_joke())
