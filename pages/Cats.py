e47fe59d9f43a187485a057e38a5afaf7ddcb300
kate <katherine.spirydovich@gmail.com>
2024-10-29 01:46:51 +0000

    huh

--- pages/Cats.py
+++ pages/Cats.py
@@ -1,8 +1,22 @@
 import streamlit as st
-import pyjokes
+# import pyjokes
 import requests
 
-st.write("# requests")
+def get_random_pokimon_image(images):
+    first_image=random.randint(1,images)
+    second_image=random.randint(1,images)
+    pictures=(first_image+second_image)
+
+
+st.image(get_random_pokimon_image(images))
+
+images = st.slider("What image will you get?", min_value=2, max_value=20)
+
+pictures = get_random_pokimon_image(images)
+
+st.write("Image:", pictures)
+
+st.button("Reroll")
 
 response = requests.get("https://pokeapi.co/api/v2/pokemon/12/")
 st.image(response.json()["sprites"]["front_default"])
