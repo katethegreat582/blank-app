2da4be7234517d04365096e92dd86c7632bb7f3d
kate <katherine.spirydovich@gmail.com>
2024-11-11 22:56:28 +0000

    hi

--- pages/Cats.py
+++ pages/Cats.py
@@ -2,10 +2,12 @@ import streamlit as st
 # import pyjokes
 import requests
 
+def get_image(num):
+    ...
+    response = requests.get("https://pokeapi.co/api/v2/pokemon/12/")
+    st.image(response.json()["sprites"]["front_default"])
 num = st.slider("", 1. 150, 25)
 
 response = requests.get("https://pokeapi.co/api/v2/pokemon/12/")
 st.image(response.json()["sprites"]["front_default"])
 
-
-# st.write(pyjokes.get_joke())
