84fd1b453f1897f5f623233da42cce2ffef6e6e1
kate <katherine.spirydovich@gmail.com>
2024-11-12 15:21:41 +0000

    yessssss

--- pages/Rain.py
+++ pages/Rain.py
@@ -4,9 +4,9 @@ from streamlit_extras import let_it_rain
 import streamlit_extras.let_it_rain
 
 streamlit_extras. let_it_rain. rain(
-    emoji="🎉",
-    font_size=54,
-    falling_speed=5,
+    emoji="🎈",
+    font_size=25,
+    falling_speed=7,
     animation_length="infinite",
 )
 
