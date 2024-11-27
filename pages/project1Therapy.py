85534eb58e3259b2f3b27836aa29f5fdf8cfa733
kate <katherine.spirydovich@gmail.com>
2024-11-27 02:04:00 +0000

    don't be descourgaed!!

--- pages/project1Therapy.py
+++ pages/project1Therapy.py
@@ -2,4 +2,19 @@ import streamlit as st
 
 st.title("Welcome to Therapy with Felica and Kate!")
 
-st.write("We hope that you can complete the following and become filled with joy and become rejuvenated")
+st.write("We hope that you can complete the following and become filled with joy and become rejuvenated!")
+
+with st.chat_message("user"):
+    st.write("If you want to experience a moment of joy click the button and see some amazing dog pictures!!")
+    st.button("Reset", type="primary")
+    if st.button("Show Dog Photos:"):
+        st.write()
+
+
+
+
+
+
+
+
+
