c7a8baa4c477dead5f3c32066f21dc82e6237983
kate <katherine.spirydovich@gmail.com>
2024-11-27 01:46:41 +0000

    oops

---
+++ pages/project1Therapy.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Welcome to Therapy with Felica and Kate!")
+
+st.write("We hope that you can complete the following and become filled with joy and become rejuvenated")
