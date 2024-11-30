32a405b1b99df2e4938b991c68f29109a955c9cc
kate <katherine.spirydovich@gmail.com>
2024-11-30 15:41:57 +0000

    almost there

--- pages/project1Therapy.py
+++ pages/project1Therapy.py
@@ -4,11 +4,18 @@ st.title("Welcome to Therapy with Felica and Kate!")
 
 st.write("We hope that you can complete the following and become filled with joy and become rejuvenated!")
 
+
+image_urls = [
+    "https://example.com/dog1.jpg",
+    "https://example.com/dog2.jpg",
+]
+
 with st.chat_message("user"):
     st.write("If you want to experience a moment of joy click the button and see some amazing dog pictures!!")
     st.button("Reset", type="primary")
-    if st.button("Show Dog Photos:"):
-        st.write()
+    if st.button("show dog Photos:"):
+            st.write(image_urls)
+
 
 
 
