4c68e80ae1269574950270a65348692ce6d5a6db
kate <katherine.spirydovich@gmail.com>
2024-12-01 01:22:54 +0000

    go

--- pages/project1Therapy.py
+++ pages/project1Therapy.py
@@ -1,7 +1,5 @@
 import streamlit as st
 
-st.link_button("show dog Photos2:", "https://hips.hearstapps.com/hmg-prod/images/little-cute-maltipoo-puppy-royalty-free-image-1652926025.jpg")
-
 st.title("Welcome to Therapy with Felica and Kate!")
 
 st.write("We hope that you can complete the following and become filled with joy and become rejuvenated!")
@@ -19,7 +17,8 @@ with st.chat_message("user"):
     st.write("If you want to experience a moment of joy click the button and see some amazing dog pictures!!")
     st.button("Reset", type="primary")
 #    if st.link_button("show dog Photos:", image_urls):
-    st.link_button("show dog Photos:", "https://www.google.com")
+#    st.link_button("show dog Photos:", "https://www.google.com")
+    st.link_button("show dog Photos:", "https://hips.hearstapps.com/hmg-prod/images/little-cute-maltipoo-puppy-royalty-free-image-1652926025.jpg")
 #            st.write("(%s)" % image_urls)
 
 
