a3c42d5a04205d6c4eb3ad5f15e8644bb9434dea
kate <katherine.spirydovich@gmail.com>
2024-12-01 01:30:14 +0000

    1

--- pages/project1Therapy.py
+++ pages/project1Therapy.py
@@ -8,8 +8,8 @@ st.write("We hope that you can complete the following and become filled with joy
 image_urls = [
 #    (https://example.com/dog1.jpg),
 #    (https://example.com/dog2.jpg)
-#     (https://hips.hearstapps.com/hmg-prod/images/little-cute-maltipoo-puppy-royalty-free-image-1652926025.jpg)
-     ("https://google.com")   
+     ('https://hips.hearstapps.com/hmg-prod/images/little-cute-maltipoo-puppy-royalty-free-image-1652926025.jpg')
+#     ("https://google.com")   
 ]
 
 
@@ -18,7 +18,9 @@ with st.chat_message("user"):
     st.button("Reset", type="primary")
 #    if st.link_button("show dog Photos:", image_urls):
 #    st.link_button("show dog Photos:", "https://www.google.com")
-    st.link_button("show dog Photos:", "https://hips.hearstapps.com/hmg-prod/images/little-cute-maltipoo-puppy-royalty-free-image-1652926025.jpg")
+#    st.link_button("show dog Photos:", "https://hips.hearstapps.com/hmg-prod/images/little-cute-maltipoo-puppy-royalty-free-image-1652926025.jpg")
+    st.link_button("show dog Photos:", image_urls[0])
+
 #            st.write("(%s)" % image_urls)
 
 
