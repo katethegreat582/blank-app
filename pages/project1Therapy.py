9c845199845227e98e61ffe644c66eb1336220da
kate <katherine.spirydovich@gmail.com>
2024-12-05 20:51:58 +0000

    1

--- pages/project1Therapy.py
+++ pages/project1Therapy.py
@@ -12,21 +12,25 @@ image_urls = [
      ('https://upload.wikimedia.org/wikipedia/commons/b/b2/Dog-1123016_960_720.jpg')
   
 ]
+st.write("####")
 
-with st.chat_message("user"):
-    st.write("If you want to experience a moment of joy click the button and see some amazing dog pictures!!")
-    st.button("Reset", type="primary")
-    st.link_button("show dog Photo 1:", image_urls[0])
-    st.link_button("show dog Photo 2:", image_urls[1])
-    st.link_button("show dog Photo 3:", image_urls[-1])
+st.write("If you want to experience a moment of joy click the button and see some amazing dog pictures!!")
 
-
+st.link_button("show dog Photo 1:", image_urls[0])
+st.link_button("show dog Photo 2:", image_urls[1])
+st.link_button("show dog Photo 3:", image_urls[-1])
 
 
-with st.expander("Click here to see options for animals up for adoption."):
-     st.image("https://static.streamlit.io/examples/dog.jpg")
+st.write("####")
+
+#with st.expander("Click here to see options for animals up for adoption."):
+#     st.image("https://static.streamlit.io/examples/dog.jpg")
      
-     
+st.write("####")
+st.write("####")
+
+
+
 st.write("Below are quotes that can encourage and energize people.")
 Bible_Quotes, John_Piper, Tim_Keller = st.tabs(["Bible", "John Piper", "Tim Keller"])
 
@@ -42,9 +46,9 @@ with Tim_Keller:
      st.write("The gospel says you are simultaneously more sinful and flawed than you ever dared believe, yet more loved and accepted than you ever dared hope.")
 
 
+st.write("####")
 
-
-st.write("Below are soothing sounds that help release anxiety and stress")
+st.write("Below is a video to help strengthen and encourage you!")
 
 st.video("https://www.youtube.com/watch?v=cbOo6lpUdlY",autoplay=True)
 
